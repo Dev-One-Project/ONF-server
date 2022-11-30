f6bfeb8bf230af53926ecb699ee960b91d085068
PackChanHyo <cksgy1510@gmail.com>
2022-11-30 18:43:57 +0900

    :sparkles: FEATURE: Vacation Entity 생성 (#11)
    FEATURE: Vacation Entity 생성
    
    closed #11

--- src/apis/vacation/entities/vacation.entity.ts
+++ src/apis/vacation/entities/vacation.entity.ts
@@ -1,5 +1,54 @@
-import { ObjectType } from "@nestjs/graphql";
-import { Entity } from "typeorm";
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
-@ObjectType()
 @Entity()
+@ObjectType()
+export class Vacation {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column()
+  @Field(() => Date)
+  vacationEnd: Date;
+
+  @Column()
+  @Field(() => Date)
+  vacationStart: Date;
+
+  @Column()
+  @Field(() => String)
+  vacationGroup: string;
+
+  @Column()
+  @Field(() => String)
+  vacationType: string;
+
+  @Column()
+  @Field(() => Int)
+  paidTime: number;
+
+  @Column()
+  @Field(() => Number)
+  deductionDays: number;
+
+  @Column()
+  @Field(() => String)
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
