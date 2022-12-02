3e5e858c7ee1e30642de91d1158df9a24e7f9568
Brian Hong <ushiii111798@gmail.com>
2022-12-02 17:43:41 +0900

    :sparkles: (category): FEATURE: category APIs & Entity
    add category entity
    add category CRUD APIs
    update company delete logic to include category and member removal
    
    closed #29

--- src/apis/categories/category.module.ts
+++ src/apis/categories/category.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyService } from '../companies/company.service';
 import { Company } from '../companies/entities/company.entity';
+import { Member } from '../members/entities/member.entity';
+import { MemberService } from '../members/member.service';
 import { CategoryResolver } from './category.resolver';
 import { CategoryService } from './category.service';
 import { Category } from './entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category, Company])],
-  providers: [CategoryResolver, CategoryService, CompanyService],
+  imports: [TypeOrmModule.forFeature([Category, Company, Member])],
+  providers: [CategoryResolver, CategoryService, CompanyService, MemberService],
 })
 export class CategoryModule {}
