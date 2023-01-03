29759caab3d58da0b048ab4f9a05fe25cfef6579
Brian Hong <ushiii111798@gmail.com>
2023-01-03 17:36:04 +0900

    :zap: (VacationIssue): MODIFY: add guard and update vacation issue
    add guard when fetch
    
    closed #149

--- src/apis/vacationIssues/vacationIssues.resolver.ts
+++ src/apis/vacationIssues/vacationIssues.resolver.ts
@@ -1,8 +1,14 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateVacationIssueInput } from './dto/createVacationissue.input';
 import { UpdateVacationIssueInput } from './dto/updateVacationissue.input';
 import { VacationIssue } from './entities/vacationIssue.entity';
 import { VacationIssuesService } from './vacationIssues.service';
+import { Roles } from 'src/common/auth/roles.decorator';
+import { Role } from 'src/common/types/enum.role';
+import { GqlAuthAccessGuard } from 'src/common/auth/gql-auth.guard';
+import { RolesGuard } from 'src/common/auth/roles.guard';
+import { IContext } from 'src/common/types/context';
 
 @Resolver()
 export class VacationIssuesResolver {
@@ -17,83 +23,91 @@ export class VacationIssuesResolver {
     return await this.vacationIssuesService.findOne({ vacationIssueId });
   }
 
+  @Roles(Role.ADMIN)
+  @UseGuards(GqlAuthAccessGuard, RolesGuard)
   @Query(() => [[VacationIssue]], {
     description: '관리자 EndDate가 기준일자인 휴가발생 조회하기',
   })
   async fetchVacationIssueBaseDate(
     @Args('baseDate') baseDate: Date,
-    @Args('companyId') companyId: string,
     @Args({ name: 'organizationId', type: () => [String] })
     organizationId: string[],
     @Args('startDate', { nullable: true }) startDate: Date,
     @Args('endDate', { nullable: true }) endDate: Date,
+    @Context() ctx: IContext,
   ) {
     return await this.vacationIssuesService.fetchVacationIssueBaseDate({
       startDate,
       endDate,
-      companyId,
+      companyId: ctx.req.user.company,
       baseDate,
       organizationId,
     });
   }
 
+  @Roles(Role.ADMIN)
+  @UseGuards(GqlAuthAccessGuard, RolesGuard)
   @Query(() => [[VacationIssue]], {
     description: '관지라 EndDate가 기준일자이고, 퇴사자랑 같이 조회',
   })
   async fetchVacationIssueWithBaseDateDelete(
     @Args('baseDate') baseDate: Date,
-    @Args('companyId') companyId: string,
     @Args({ name: 'organizationId', type: () => [String] })
     organizationId: string[],
     @Args('startDate', { nullable: true }) startDate: Date,
     @Args('endDate', { nullable: true }) endDate: Date,
+    @Context() ctx: IContext,
   ) {
     return await this.vacationIssuesService.fetchVacationIssueWithBaseDateDelete(
       {
         startDate,
         endDate,
-        companyId,
+        companyId: ctx.req.user.company,
         baseDate,
         organizationId,
       },
     );
   }
 
+  @Roles(Role.ADMIN)
+  @UseGuards(GqlAuthAccessGuard, RolesGuard)
   @Query(() => [[VacationIssue]], {
     description: '관리자 기준일자 기준으로 앞에 발생된 휴가 목록 조회',
   })
   async fetchVacationIssueDetailDate(
     @Args('baseDate') baseDate: Date,
-    @Args('companyId') companyId: string,
     @Args({ name: 'organizationId', type: () => [String] })
     organizationId: string[],
     @Args('startDate', { nullable: true }) startDate: Date,
     @Args('endDate', { nullable: true }) endDate: Date,
+    @Context() ctx: IContext,
   ) {
     return await this.vacationIssuesService.findWithDetailDate({
       startDate,
       endDate,
-      companyId,
+      companyId: ctx.req.user.company,
       baseDate,
       organizationId,
     });
   }
 
+  @Roles(Role.ADMIN)
+  @UseGuards(GqlAuthAccessGuard, RolesGuard)
   @Query(() => [[VacationIssue]], {
     description: '관리자 기준일자 기준으로 앞에 발생된 휴가 퇴사자랑 같이 조회',
   })
   async fetchVacationIssueDetailDateDelete(
     @Args('baseDate') baseDate: Date,
-    @Args('companyId') companyId: string,
     @Args('startDate', { nullable: true }) startDate: Date,
     @Args('endDate', { nullable: true }) endDate: Date,
     @Args({ name: 'organizationId', type: () => [String] })
     organizationId: string[],
+    @Context() ctx: IContext,
   ) {
     return await this.vacationIssuesService.findWithDetailDateDelete({
       startDate,
       endDate,
-      companyId,
+      companyId: ctx.req.user.company,
       baseDate,
       organizationId,
     });
