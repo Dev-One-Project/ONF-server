aa55a2b41934b0512daae52fa877488b6c9849c1
PackChanHyo <cksgy1510@gmail.com>
2023-01-09 18:51:45 +0900

    :zap: MODIFY: vacation APIs 수정 #178
    MODIFY: vacation APIs 수정 #178
    
    Vacation fetch update
    Vacation create organization add update
    VacationIssue create organization add update
    VacationCategory create organization add update
    
    closed #178

--- src/apis/vacationCategory/vacationCategory.module.ts
+++ src/apis/vacationCategory/vacationCategory.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Organization } from '../organization/entities/organization.entity';
+import { RoleCategory } from '../roleCategory/entities/roleCategory.entity';
 import { VacationCategory } from './entities/vacationCategory.entity';
 import { VacationCategoryResolver } from './vacationCategory.resolver';
 import { VacationCategoryService } from './vacationCategory.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([VacationCategory])],
+  imports: [
+    TypeOrmModule.forFeature([VacationCategory, Organization, RoleCategory]),
+  ],
   providers: [
     VacationCategoryService, //
     VacationCategoryResolver,
