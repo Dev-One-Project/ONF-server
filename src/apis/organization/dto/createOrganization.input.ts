0b16468228629792b8b6cefc0ee6d98e94b32efb
Brian Hong <ushiii111798@gmail.com>
2023-01-09 18:54:54 +0900

    :zap: (Organization): MODIFY: update organization column
    remove color
    add checkPoint, range
    
    closed #181

--- src/apis/organization/dto/createOrganization.input.ts
+++ src/apis/organization/dto/createOrganization.input.ts
@@ -1,9 +1,12 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 
 @InputType()
 export class CreateOrganizationInput {
   @Field(() => String, { nullable: false })
   name: string;
+
+  @Field(() => String, { nullable: true })
+  checkPoint: string;
 
   @Field(() => String, { nullable: false })
   address: string;
@@ -13,6 +16,9 @@ export class CreateOrganizationInput {
 
   @Field(() => String, { nullable: true })
   lng: string;
+
+  @Field(() => Int, { nullable: true })
+  range: number;
 
   @Field(() => String, { nullable: true })
   description: string;
