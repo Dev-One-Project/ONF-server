9c2367a7efdd3a8c53e6ceae8ce851f034e3cb36
Hoon2 <gooaba1204@gmail.com>
2023-01-10 22:46:31 +0900

    ⚡ (workCheck): MODIFY: workCheck APIs 수정 (#199)

--- src/apis/schedules/schedule.service.ts
+++ src/apis/schedules/schedule.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  NotImplementedException,
-  UnprocessableEntityException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Member } from '../members/entities/member.entity';
